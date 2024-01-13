ae06467a245e7341439952edac2ec1a66f0b0898
Игорь Скрипников <54932391+spring108@users.noreply.github.com>
2024-01-13 21:40:48 +0300

    Update FormSave.java

--- src/com/shephertz/app42/paas/sample/FormSave.java
+++ src/com/shephertz/app42/paas/sample/FormSave.java
@@ -65,6 +65,10 @@ public class FormSave extends HttpServlet {
 			PrintWriter out = response.getWriter();
 			out.print("<!doctype html><html><head><meta charset='utf-8'><title>App42 Sample Java-MySql Application</title><link href='css/style-User-Input-Form.css' rel='stylesheet' type='text/css'></head><body><div class='App42PaaS_header_wrapper'><div class='App42PaaS_header_inner'><div class='App42PaaS_header'><div class='logo'><a href='http://app42paas.shephertz.com'><img border='0' alt='App42PaaS' src='images/logo.png'></img></a></div></div></div></div><div class='App42PaaS_body_wrapper'><div class='App42PaaS_body'><div class='App42PaaS_body_inner'><div class='contactPage_title'>");
 			out.print("<h2 align='center'>Error occured. See Logs.</h2><br/><br/>");
+			out.print("getMessage:<br>");
+			out.print(ex.getMessage());
+			out.print("toString:<br>");
+			out.print(ex.toString());
 			out.print("<br/><a href='/' style='font-size: 18px;'>Back</a>");
 			out.print("</div></div></div></div></body></html>");
 		}
